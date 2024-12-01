e50bc6b24fe054326fa381775399a234f8474f57
Nilanshu Basnet <mail.nilanshu@gmail.com>
2024-12-01 19:04:27 +1100

    Added User Input Section

--- src/components/MemoryGame.jsx
+++ src/components/MemoryGame.jsx
@@ -1,37 +1,51 @@
-import React, { useState } from 'react'
-
+import React, { useState } from "react";
 
 const MemoryGame = () => {
+  const [gridSize, setGridSize] = useState(4);
 
-  const [gridSize, setGridSize] = useState(4)
-  
+  const [cards, setCards] = useState([]);
+  const [flipped, setFlipped] = useState([]);
+  const [solved, setSolved] = useState([]);
+
+  const [disabled, setDisabled] = useState(false);
+  const [won, setWon] = useState(false);
+
+  const handleSizeChange = (e) => {
+    const size = parseInt(e.target.value);
+
+    if (size >= 2 && size <= 10) {
+      setGridSize(size);
+    }
+  };
+
   return (
-    <div className='flex h-screen w-screen'>
-        <div className='h-full w-[40%] flex flex-col items-center justify-center'>
-            <h1 className='font-bold text-2xl'>Memory Game</h1>
-            <img className='w-[50%]' src="/gameAIlogo.JPG" alt="" />
+    <div className="flex h-screen w-screen bg-[#ECF0F9]">
+      <div className="h-full w-[40%] flex flex-col items-center justify-center">
+        <img className="w-[80%] mb-10" src="/gameAIlogo.JPG" alt="" />
 
-            <div>
-              <label 
-              className='mr-2'
-              htmlFor="gridSize">Grid Size:</label>
+        <div>
+          <label className="mr-3 text-2xl font-semibold" htmlFor="gridSize">
+            Grid Size:
+          </label>
 
-              <input 
-              className='bg-gray-100 pl-2 rounded'
-              id="gridSize"
-              type="number" 
-              value={gridSize} 
-              min="2" 
-              max="10"/>
-            </div>
-            <button className='bg-emerald-500 text-white py-2 px-8 rounded mt-5'>Play</button>
+          <input
+            className="bg-white px-2 py-2  rounded text-2xl"
+            id="gridSize"
+            type="number"
+            value={gridSize}
+            min="2"
+            max="10"
+            onChange={handleSizeChange}
+          />
         </div>
+        <button className="bg-emerald-500 hover:bg-emerald-400 text-white text-2xl py-2 px-10 rounded mt-10">
+          Play
+        </button>
+      </div>
 
-        <div className='bg-emerald-600 h-full w-[60%]'>
-            Partition 2
-        </div>
+      <div className="bg-emerald-600 h-full w-[60%] rounded">Partition 2</div>
     </div>
-  )
-}
+  );
+};
 
-export default MemoryGame
+export default MemoryGame;
